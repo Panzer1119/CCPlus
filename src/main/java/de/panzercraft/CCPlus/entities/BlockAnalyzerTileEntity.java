188e33111bba12141593c3c04b6145955fa409c5
Paul Hagedorn <panzer1119@gmail.com>
2016-12-03 17:57:29 +0100

    CC support for the block analyzer

--- src/main/java/de/panzercraft/CCPlus/entities/BlockAnalyzerTileEntity.java
+++ src/main/java/de/panzercraft/CCPlus/entities/BlockAnalyzerTileEntity.java
@@ -1,14 +1,52 @@
 package de.panzercraft.CCPlus.entities;
 
+import dan200.computercraft.api.lua.ILuaContext;
+import dan200.computercraft.api.lua.LuaException;
+import dan200.computercraft.api.peripheral.IComputerAccess;
+import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
-public class BlockAnalyzerTileEntity extends TileEntity {
+public class BlockAnalyzerTileEntity extends TileEntity implements IPeripheral {
 	
 	private World world;
 	
 	public BlockAnalyzerTileEntity(World world) {
 		this.world = world;
+	}
+
+	@Override
+	public String getType() {
+		return "block_anaylzer";
+	}
+
+	@Override
+	public String[] getMethodNames() {
+		return new String[] {}; 
+	}
+
+	@Override
+	public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException {
+		switch(method) {
+			default:
+				return new Object[] {};
+		}
+	}
+
+	@Override
+	public void attach(IComputerAccess computer) {
+
+	}
+
+	@Override
+	public void detach(IComputerAccess computer) {
+
+		
+	}
+
+	@Override
+	public boolean equals(IPeripheral other) {
+		return other == this && other instanceof BlockAnalyzerTileEntity;
 	}
 
 }
