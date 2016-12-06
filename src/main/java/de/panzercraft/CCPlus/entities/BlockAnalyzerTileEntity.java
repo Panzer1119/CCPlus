622ada7f00a1653729430e2fefc257a00e853089
Paul Hagedorn <panzer1119@gmail.com>
2016-12-06 18:06:27 +0100

    BlockAnalyzer can now search for blocks

--- src/main/java/de/panzercraft/CCPlus/entities/BlockAnalyzerTileEntity.java
+++ src/main/java/de/panzercraft/CCPlus/entities/BlockAnalyzerTileEntity.java
@@ -1,5 +1,6 @@
 package de.panzercraft.CCPlus.entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cpw.mods.fml.common.registry.GameData;
@@ -9,6 +10,8 @@ import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 import de.panzercraft.CCPlus.CCPlus;
+import de.panzercraft.CCPlus.blocks.BlockPos;
+import de.panzercraft.CCPlus.blocks.BlockPosExact;
 import de.panzercraft.CCPlus.utils.MathPlus;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
@@ -32,7 +35,7 @@ public class BlockAnalyzerTileEntity extends TileEntity implements IPeripheral {
 
 	@Override
 	public String[] getMethodNames() {
-		return new String[] {"getBlock"}; 
+		return new String[] {"getBlock", "isBlockInRange", "getBlocks"}; 
 	}
 
 	@Override
@@ -50,7 +53,8 @@ public class BlockAnalyzerTileEntity extends TileEntity implements IPeripheral {
 					world_2 = MinecraftServer.getServer().worldServerForDimension(dim);
 				}
 				if(CCPlus.block_analyzer_enable_dimensional_analysis || dim == world.provider.dimensionId) {
-					if(!isInRange(x, y, z)) {
+					boolean inRange = isInRange(x, y, z);
+					if(!inRange) {
 						return new Object[] {"Out of range"};
 					} else {
 						if(CCPlus.block_analyzer_enable_dimensional_analysis && dim != world.provider.dimensionId && world_2 == null) {
@@ -70,6 +74,72 @@ public class BlockAnalyzerTileEntity extends TileEntity implements IPeripheral {
 					return new Object[] {"Multidimensional block analysis isnt supported"};
 				}
 				return new Object[] {data};
+			case 1:
+				String block_unlocalizedName = ((String) arguments[0]);
+				int block_range = ((Number) arguments[1]).intValue();
+				int minx = this.xCoord - block_range;
+				int maxx = this.xCoord + block_range;
+				int miny = this.yCoord - block_range;
+				int maxy = this.yCoord + block_range;
+				int minz = this.zCoord - block_range;
+				int maxz = this.zCoord + block_range;
+				BlockPos minpos = new BlockPos(minx, miny, minz);
+				BlockPos maxpos = new BlockPos(maxx, maxy, maxz);
+				boolean found = false;
+				for(int x2 = minx; x2 <= maxx; x2++) {
+					for(int y2 = miny; y2 <= maxy; y2++) {
+						for(int z2 = minz; z2 <= maxz; z2++) {
+							BlockPos pos = new BlockPos(x2, y2, z2);
+							Block block = world.getBlock(pos.x, pos.y, pos.z);
+							if(block != null) {
+								if(block.getUnlocalizedName().equals(block_unlocalizedName)) {
+									found = true;
+								}
+							}
+							if(found) {
+								break;
+							}
+						}
+						if(found) {
+							break;
+						}
+					}
+					if(found) {
+						break;
+					}
+				}
+				return new Object[] {found};
+			case 2:
+				String block_unlocalizedName_2 = ((String) arguments[0]);
+				int block_range_2 = ((Number) arguments[1]).intValue();
+				int minx_2 = this.xCoord - block_range_2;
+				int maxx_2 = this.xCoord + block_range_2;
+				int miny_2 = this.yCoord - block_range_2;
+				int maxy_2 = this.yCoord + block_range_2;
+				int minz_2 = this.zCoord - block_range_2;
+				int maxz_2 = this.zCoord + block_range_2;
+				BlockPos minpos_2 = new BlockPos(minx_2, miny_2, minz_2);
+				BlockPos maxpos_2 = new BlockPos(maxx_2, maxy_2, maxz_2);
+				ArrayList<HashMap<String, Object>> positions = new ArrayList<HashMap<String, Object>>();
+				for(int x2 = minx_2; x2 <= maxx_2; x2++) {
+					for(int y2 = miny_2; y2 <= maxy_2; y2++) {
+						for(int z2 = minz_2; z2 <= maxz_2; z2++) {
+							BlockPos pos = new BlockPos(x2, y2, z2);
+							Block block = world.getBlock(pos.x, pos.y, pos.z);
+							if(block != null) {
+								if(block.getUnlocalizedName().equals(block_unlocalizedName_2)) {
+									HashMap<String, Object> temp = new HashMap<String, Object>();
+									temp.put("x", pos.x);
+									temp.put("y", pos.y);
+									temp.put("z", pos.z);
+									//temp.put("dim", world.provider.dimensionId);
+									positions.add(temp);
+								}
+							}
+						}
+					}
+				}
+				return new Object[] {positions};
 			default:
 				return new Object[] {};
 		}
